327bb8d52c263aa6de9de8126941b0b20afb3da9
w5b <30703153+w5b@users.noreply.github.com>
2023-12-20 19:39:25 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,155 @@
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, D M M \n
+\n\n
+ASCENDED COMMANDER : \n
+RZ, NMS, NH, MDB, JS, JA, FC, RB, VR, MDG, RH, PLC, FOX, SN, OYI, 
+WG, ES, MQ, RD, BK, NK, TT, AC, RP, IV, JX, RS, AKM, FRT, SP, VL, 
+FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM, 
+CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR, EZP,
+AZ, CB, DY, KM, ACT, PM, AB, SX, AB, RP, DN, NN, AS, AF, VM, TS, CT,
+KG, TX, DP\n
+============================\n
+that name displayed is the donator\n
+if you donator and want specific nickname, just let me know\n
+consider donating to keep this project alive\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+              
+uuid_saved = ["9aeaaceef9","0d5192aaa8","2d3300c56b","79941dabfb","1bcd371a24","8ccb5171bc","48e98a00fb","fc5966ec43","6753ae7c20","367e483576","701deed7a1","c02d618675","0c28dc0eb4","5044c0deb2","3afeaf99ff","3837ba6f30","97f88c63d7","9756b3c68e","91549412a3","c009ac19cc","76283757c3","1433e3fbf0","596851a385","28ca2077b2","0e67f2ac77","ccda0f3c76","784d42e8b8","7da945130a","bc399d7191","c155f9c7d4","d2e264842a","0f807d59e8","7457621e28","3034d3fdad","91bc6add19","7fe8a30c36","fec12d3dfe","45c31f67d6","54d6f759c8","27ad43a152","8bdb6dba2f","40dc55a18b","2402f332f3","4b49224938","05c45dae6a","602b7b391b","1af7f3316f","613007de10","c178ab4972","6a5db5599a","fee0c0b244","97c795658d","cad32eb6ab","0f15f4702e","613007de10","c3d30ec84c","f151b202dc","11100ece8b","a57c519973","199879ef8d","a014c6f860","b7c0fe27f5","2bdc737541","06dc312134","93582e4eae","026726e0eb","ecc010a66e","d461de7177","96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","e53d258881","75a4159f86","10808ae13a","01035bf5b7","68085ef0f0","8251db86c5","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","3534d65702","x5eb7799781","090afbe976","7e10425e1b","c34b37dfd9","d6fbdc1520","31607ab4c7","5debc04d27","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0","665b24b4cb","2ba0d93345","2e1d27765c","f25a6280ce"]
+
+
+
+
+'''
+2d3300c56b = Doople
+79941dabfb = TUX1337
+8ccb5171bc = Knallgurke 2
+48e98a00fb = Knallgurke
+fc5966ec43 = Critias
+701deed7a1 = TSAO
+367e483576 = TSAO 2
+0c28dc0eb4 = Afflictedfury
+5044c0deb2 = Astro
+97f88c63d7 = Nossninja
+c009ac19cc = Don
+596851a385 = Røpke
+28ca2077b2 = ASTRO 80Y
+0e67f2ac77 = Shaxen
+ccda0f3c76 = Articvb
+1433e3fbf0 = Articvb 2
+784d42e8b8 = Private Marx
+7da945130a = Alcatraz 
+bc399d7191 = Kilmanagh
+c155f9c7d4 = dokyun
+d2e264842a = colebear
+7457621e28 = AzRa
+3034d3fdad = EdZoPlay
+91bc6add19 = TheTwork Sveik584
+7fe8a30c36 = Steryan
+fec12d3dfe = xtraRice
+45c31f67d6 = Bolt
+54d6f759c8 = Cekko
+27ad43a152 = TheTwork
+8bdb6dba2f = RenjaHB
+40dc55a18b = FamousFarrow
+2402f332f3 = Morticus
+4b49224938 = Sam Silva
+05c45dae6a = Tiger
+1af7f3316f = sasquash
+613007de10 = Daunt
+3778c59307 = LordVader (payment pending Thursday) not pay, removed
+6a5db5599a = Dutchwise
+fee0c0b244 = HiGW
+0f807d59e8 = Cekcenk
+97c795658d = Cekcenk new vm 
+cad32eb6ab =  jiimmy
+0f15f4702e = Biscotti
+613007de10 = RdN
+c178ab4972 = No62NoExit 2 laptop
+f151b202dc = No62NoExit
+91549412a3 = No62NoExit 3
+3afeaf99ff = No62NoExit 4 
+9756b3c68e = Lucas--MileFantazy old disable
+11100ece8b = Lucas--MileFantazy
+9756b3c68e = Lucas--MileFantazy 2
+3837ba6f30 = Lucas--MileFantazy 2
+a57c519973 = Taldor
+199879ef8d = akull123456
+1ff58644c8 = TheHacker old off
+a014c6f860 = TheHacker
+b7c0fe27f5 = Mizunie
+1bcd371a24 = Mizunie 2
+0d5192aaa8 = Mizunie 3
+9aeaaceef9 = Mizunie 4
+06dc312134 = NJ
+93582e4eae = khicon
+d461de7177 = Level 1 Crook
+602b7b391b = Level 1 Crook 2
+76283757c3 = Level 1 Crook 3
+96029128c9 = carrjam
+f251ad584e = Error
+6b6c5a4f88 = Dumnezeus
+b679c63915 = Luxz
+9ce4111361 = Freq
+e53d258881 = Villain
+75a4159f86 = SquishyPoo1870
+10808ae13a = FRTZ
+c02d618675 = FRTZ 2
+01035bf5b7 = Akemi
+8a3ebb6c8b = Robsie Remove
+8251db86c5 = Robsie New
+f106ab283d = J4XXXOR
+f89f95cbcc = ivicask
+c3d30ec84c = ivicask 2
+da698b0e4c = RarePepe
+d7e53d75f2 = Aiden Cross 
+3496ec8c63 = tiredtbh
+e2823149be = Naksu
+01035bf5b7 = Beutelkramer
+e84e400c61 = ridicul0us
+2bdc737541 = Morqitta 3
+68085ef0f0 = Morqitta 2
+9e87fbea81 = Morqitta new
+33bfcbbadc = EddieSmurphy
+d35d2ec041 = wongo
+2bd4280a7d = Truckingsteve#007
+df33e92ff7 =  OD YR INK off
+3534d65702 = Kaisen OD YR INK
+5eb7799781 = Syrix == not confirmed, i add x on the id
+090afbe976 = Sinic
+7e10425e1b = フォックスマン
+2ffeb1091c = polychromatik off
+c34b37dfd9 = polychromatik new
+d6fbdc1520 = Reeeehaww
+0f407ed34e = Mindaugas OFF
+31607ab4c7 = Mindaugas New
+5debc04d27 = Hawaiian
+da2569d097 = Varek
+30fac06e50 = falco
+6fe74d85f3 = Jaxx Armstrong
+ecc010a66e = don't mind me 3
+b275bc6551 = don't mind me 2
+53b625f3d9 = don't mind me
+d05112dc9e = morbiddarkblood
+cf80d1275b = NtajHmoob
+f4437fc8fe = nemesis
+6753ae7c20 = Nemesiss 2
+3678bde5e3 = intel_power
+2fda4ec1b0 = kowa ( GIVE ACCOUNT TO Robbes )
+d1ee7c0888 = Robbes
+d72f0e4f08 = rietzee
+
+f25a6280ce = sabay
+2ba0d93345 = VM 1
+026726e0eb = vm 2
+665b24b4cb = VM 3
+2e1d27765c = VM 4
+'''
+#=============
+#==============
